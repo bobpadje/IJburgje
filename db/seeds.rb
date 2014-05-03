08157eb911cba7b7e5d7e2d7bdd865fffd2e8952
Jordan Salazar <jordan.salazar@hva.nl>
2014-05-03 16:53:36 +0200

    emails toegevoegd

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,11 @@
 
 u = User.create :email => 'jrdnsalazar@gmail.com', :name => 'Jordan Salazar'
 u.save
+u = User.create :email => 'bobpadje@gmail.com', :name => 'Bob van t Padje'
+u.save
+u = User.create :email => 'cassetproductions@gmail.com', :name => 'Nick Zwaans'
+u.save
+u = User.create :email => 'r-h_rami@live.nl', :name => 'Rami Haddad'
+u.save
+u = User.create :email => 'christiaanwalters@gmail.com', :name => 'Christiaan Walters'
+u.save
